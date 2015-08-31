266032ed6b02ca9775332ac05601395b2778741f
Paul Brescia <brescia717@gmail.com>
2015-08-31 13:58:33 -0400

    Added mailcatcher gem and updated config/environments/development.rb accordingly

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,16 +18,17 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
   # This needs modification for user credentials.
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: '587',
-    enable_starttls_auto: true,
-    user_name: 'someuser',
-    password: 'somepass',
-    authentication: :plain,
-    domain: 'http://127.0.0.1:3000'
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'gmail.com',
+  #   user_name:            'brescia717',
+  #   password:             'password',
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
