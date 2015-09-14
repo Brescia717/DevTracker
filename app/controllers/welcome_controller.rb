2ff0ee199c9f79349981d053707480b8851ae6e1
Paul Brescia <brescia717@gmail.com>
2015-09-14 13:58:58 -0400

    Added inclusion of ProfileHelper

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
+  include ProfileHelper
+
   def index
     @profiles = Profile.all
     gon.allProfiles = Map.new.get_welcome_markers(@profiles)
-    @frameworks = Profile.all_frameworks
+    @frameworks = ProfileHelper.available_frameworks
   end
 end
