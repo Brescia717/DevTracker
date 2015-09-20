48636c6fec06547c08f8882d170e5508dc296b6d
Paul Brescia <brescia717@gmail.com>
2015-09-20 18:19:01 -0400

    Added comment for why use_transactional_fixtures is set to false

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,11 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false # was true
+
+  # changed to false to have Selenium recognize FactoryGirl/Capybara variables
+  # (i.e. user = create(:user))
+  config.use_transactional_fixtures = false
+
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
