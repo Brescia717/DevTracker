6da3d402ab751b1cc7313cb43141a4ecc78037c6
Paul Brescia <brescia717@gmail.com>
2015-10-07 17:25:18 -0400

    1. Renamed user_log_in_with_twitter_spec.rb to user_log_in_spec.rb 2. Added scenario for twitter and devise login and login failure 3. Modified support/form_helpers.rb to have register_account method

--- spec/support/form_helpers.rb
+++ spec/support/form_helpers.rb
@@ -6,4 +6,18 @@ module FormHelpers
   def sign_up
     find(:xpath, "//a[@href='/users/sign_in']").click
   end
+
+  def register_account
+    @user = create(:user)
+    visit root_path
+    sign_up # new_user_session_path
+
+    fill_in("Email", with: @user.email)
+    fill_in("Password", with: @user.password)
+    click_button "Log in"
+    expect(page).to have_content "You have to confirm your email address before continuing." #"Signed in successfully."
+    expect(last_email).to have_content @user.email
+    open_last_email
+    click_first_link_in_email
+  end
 end
