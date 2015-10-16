46b470e1514f8cfe9834b0eaf19aabc673b7f86a
Paul Brescia <brescia717@gmail.com>
2015-10-16 17:36:01 -0400

    Added authorization for user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,7 @@ class UsersController < ApplicationController
 
   # GET/PATCH /users/:id/finish_signup
   def finish_signup
-    # authorize! :update, @user
+    authorize! :update, @user
     if request.patch? && params[:user] && params[:user][:email]
       if @user.update(user_params)
         @user.skip_reconfirmation!
