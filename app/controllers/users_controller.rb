078b8b640aa041e507403de9652d24fff3432b03
Paul Brescia <brescia717@gmail.com>
2015-10-16 17:51:48 -0400

    Commented out authorization for user during finish_signup; added redirect to root_path on fail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,13 +58,14 @@ class UsersController < ApplicationController
 
   # GET/PATCH /users/:id/finish_signup
   def finish_signup
-    authorize! :update, @user
+    # authorize! :update, @user
     if request.patch? && params[:user] && params[:user][:email]
       if @user.update(user_params)
         @user.skip_reconfirmation!
         sign_in(@user, :bypass => true)
         redirect_to @user, notice: 'Your profile was successfully updated.'
       else
+        redirect_to root_path
         @show_errors = true
       end
     end
