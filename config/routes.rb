458d7090f63a535701957664bc6363f5d5e2da2f
Paul Brescia <brescia717@gmail.com>
2016-02-02 17:07:22 -0500

    1. Added 'name' field for user registration 2. Created registrations_controller.rb for custom devise user sanitization 3. Added resource for registrations_controller to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, :controllers => {
+    omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'
+  }
 
   resources :welcome, only: :index
   resources :users
