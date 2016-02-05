5d335852a44fe6881f01ac2fff6c8b9893bb914e
Paul Brescia <brescia717@gmail.com>
2016-02-05 10:31:15 -0500

    Added conversations to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   resources :welcome, only: :index
   resources :users
   resources :profiles
-
+  resources :conversations, only: [:index, :show, :destroy]
+  
   get   '/users/:id/settings' => 'users#settings', as: :user_settings
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   match '/auth/:provider/callback', to: 'sessions#create', via: 'get'
