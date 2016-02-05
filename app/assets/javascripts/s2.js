e09dfcee4974e1fe693d1a508812d885abba9040
Paul Brescia <brescia717@gmail.com>
2016-02-05 15:58:26 -0500

    Cleaned up code

--- app/assets/javascripts/s2.js
+++ app/assets/javascripts/s2.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
-    $( '.s2' ).select2({
-      // theme: "bootstrap",
-      placeholder: "Select users",
-      allowClear: true
-    });
-        // theme: "bootstrap",
-        // placeholder: "Select a State",
-        // allowClear: true
-    // });
+  $('.s2').select2({
+    // theme: "bootstrap",
+    placeholder: "Select users",
+    allowClear: true
+  });
 });
