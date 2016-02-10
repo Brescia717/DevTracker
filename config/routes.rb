ebb0b9cf647282b1f296b7f20c500bd8899a337d
Paul Brescia <brescia717@gmail.com>
2016-02-10 13:42:46 -0500

    Added reply path for conversations

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,13 @@ Rails.application.routes.draw do
   resources :welcome, only: :index
   resources :users
   resources :profiles
-  resources :conversations, only: [:index, :show, :destroy]
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :reply
+    end
+  end
   resources :messages, only: [:new, :create]
-  
+
   get   '/users/:id/settings' => 'users#settings', as: :user_settings
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   match '/auth/:provider/callback', to: 'sessions#create', via: 'get'
